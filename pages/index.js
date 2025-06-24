63822c77724a6c6a69aad42d13abdcb8ce5cbdf1
Caldiske88 <luca.caldara.1988@gmail.com>
2025-06-24 22:35:17 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import PhotoWall from '../components/PhotoWall';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{textAlign: 'center'}}>Sito Web Foto Anonime</h1>
+      <PhotoWall />
+    </div>
+  );
+}
