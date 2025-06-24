08fc4114655869ba1a15ba6cb15892858dce0cfe
Caldiske88 <luca.caldara.1988@gmail.com>
2025-06-24 23:07:28 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
+import Link from 'next/link';
 import PhotoWall from '../components/PhotoWall';
 
 export default function Home() {
   return (
     <div>
       <h1 style={{textAlign: 'center'}}>Sito Web Foto Anonime</h1>
+      <div style={{ display:'flex', justifyContent: 'center', margin: 16 }}>
+        <Link href="/upload">
+          <button style={{
+            background: '#185cff',
+            color: '#fff',
+            border: 0,
+            borderRadius: 6,
+            padding: '10px 24px',
+            fontSize: 18,
+            cursor: 'pointer',
+            boxShadow:'0 1px 6px #0001'
+          }}>
+            Carica Foto
+          </button>
+        </Link>
+      </div>
       <PhotoWall />
     </div>
   );
